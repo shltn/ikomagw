e99078497e552a4693b80c4e5702d92c02165ccb
shltn <kawanaka.shogo.kp1@is.naist.jp>
2017-01-21 14:39:53 +0900

    import lml

--- Ikome_cesium-starter-app-master/Source/App.js
+++ Ikome_cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('Ikoma_sample_matsuda.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://shltn.github.io/ikomagw/ikomagw/Ikome_cesium-starter-app-master/Source/Ikoma_sample_matsuda.kml'));

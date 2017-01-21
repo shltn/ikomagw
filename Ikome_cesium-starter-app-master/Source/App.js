b3e01e366961048dcbe08af0b09953dc78706ece
shltn <kawanaka.shogo.kp1@is.naist.jp>
2017-01-21 15:50:55 +0900

    change

--- Ikome_cesium-starter-app-master/Source/App.js
+++ Ikome_cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('https://shltn.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/Ikoma_sample_matsuda.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://shltn.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/civictechparty.kml'));

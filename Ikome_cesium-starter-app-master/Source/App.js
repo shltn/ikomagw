f0f0ce7350d9992a80e94e5f55894cf8c9cd7a82
shltn <kawanaka.shogo.kp1@is.naist.jp>
2017-01-22 16:10:46 +0900

    add start position

--- Ikome_cesium-starter-app-master/Source/App.js
+++ Ikome_cesium-starter-app-master/Source/App.js
@@ -2,4 +2,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.dataSources.add(Cesium.KmlDataSource.load('https://shltn.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/civictechparty.kml'));
 
-viewer.flyTo({destination : Cesium.Cartesian3.fromDegrees(135.698525,34.6918934,8000.0)});
+viewer.camera.flyTo({destination : Cesium.Cartesian3.fromDegrees(135.698525,34.6918934, 8000.0)});
